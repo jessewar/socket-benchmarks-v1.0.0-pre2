a5959286d98284f6678171a122986b385dd8f95c
Jesse Warren <jesse.warren@live.com>
2014-05-30 14:54:25 -0700

    added transport parameter to run()

--- bench.js
+++ bench.js
@@ -1,10 +1,6 @@
 var server = require('http').Server();
 var Benchmark = require('benchmark');
 var ioc = require('socket.io-client');
-var io = require('socket.io')(server, {
-  transports: ['polling', process.argv[2]] // need polling for initial transport
-});
-var version = "1.0.0";
 
 // creates a socket.io client for the given server
 function client(srv, nsp, opts) {
@@ -18,35 +14,38 @@ function client(srv, nsp, opts) {
   return ioc.connect(url, opts);
 }
 
+function run(transport) {
+  var io = require('socket.io')(server, {
+    transports: ['polling', transport] // need polling for initial transport
+  });
 
-io.sockets.on('connection', function (socket) {
-  function jsonRoundtrip(deferred) {
-    socket.emit('server-message', {
-      name: "Server"
-    });
-    socket.on('client-message', function (data) {
-      deferred.resolve();
-      socket.removeAllListeners('client-message');
-    });
-  }
+  io.sockets.on('connection', function (socket) {
+    function jsonRoundtrip(deferred) {
+      socket.emit('server-message', {
+	name: "Server"
+      });
+      socket.on('client-message', function (data) {
+	deferred.resolve();
+	socket.removeAllListeners('client-message');
+      });
+    }
 
-  var options = {
-    onStart: function () {
-      console.log("Testing SocketIO v" + version + " using " + process.argv[2] + "...");
-    },
-    onComplete: function () {
-      console.log("Mean time for JSON message from server to client back to server: " + this.stats.mean + "\n" +
-                  "Number of trips per second: " + this.hz);
-      process.exit();
-    },
-    defer: true,
-    async: true
-  };
-  var bench = new Benchmark('roundtrip', jsonRoundtrip, options);
-  bench.run();
-});
+    var options = {
+      onStart: function () {
+	console.log("Testing SocketIO v1.0.0-pre2 using " + process.argv[2] + "...");
+      },
+      onComplete: function () {
+	console.log("Mean time for JSON message from server to client back to server: " + this.stats.mean + "\n" +
+                    "Number of trips per second: " + this.hz);
+	process.exit();
+      },
+      defer: true,
+      async: true
+    };
+    var bench = new Benchmark('roundtrip', jsonRoundtrip, options);
+    bench.run();
+  });
 
-function run() {
   // connect to server triggering benchmark to start
   var clientSocket = client(server);
   clientSocket.on('server-message', function (data) {
